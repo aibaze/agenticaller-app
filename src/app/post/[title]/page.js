69b1065ec58c79e2d951c3e95b642dddecbb104b
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-03-30 17:28:39 +0700

    fix rerender

--- src/app/post/[title]/page.js
+++ src/app/post/[title]/page.js
@@ -18,11 +18,8 @@ export default function PostDetailsPage({ params }) {
 }
 
 export async function generateStaticParams() {
-  const res = await axios.get(endpoints.post.list);
 
-  return res.data.posts?.map((post) => ({
-    title: paramCase(post.title),
-  })) || []
+  return  []
 }
 
 PostDetailsPage.propTypes = {
